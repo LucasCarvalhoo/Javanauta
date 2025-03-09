25fd820e42754194f9c0acfd2d51bd5d3b375e11
LucasCarvalhoo <lucas.carvalho7933@gmail.com>
2025-03-08 21:53:01 -0300

    criar entidades e estruturar o relacionamento com usuario

--- src/main/java/com/carvalho/aprendendospring/infrastructure/entity/Usuario.java
+++ src/main/java/com/carvalho/aprendendospring/infrastructure/entity/Usuario.java
@@ -1,16 +1,17 @@
-package com.carvalho.aprendendospring;
+package com.carvalho.aprendendospring.infrastructure.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "usuario")
 public class Usuario {
 
     @Id
@@ -22,4 +23,11 @@ public class Usuario {
     private String email;
     @Column(name = "senha", length = 20)
     private String senha;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id")
+    private List<Endereco> enderecos;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id")
+    private List<Telefone> telefones;
+
 }
---
+++ src/main/java/com/carvalho/aprendendospring/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,8 @@
+package com.carvalho.aprendendospring.infrastructure.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository {
+}
