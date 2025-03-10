b58a3d1bb18b56515de061d67a7195e90896a81c
Lucas Carvalho <lucas.carvalho7933@gmail.com>
2025-03-10 15:15:03 -0300

    implementa dto para exibir email e senha do usuario

---
+++ src/main/java/com/carvalho/aprendendospring/dtos/UsuarioDTO.java
@@ -0,0 +1,13 @@
+package com.carvalho.aprendendospring.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioDTO {
+    private String email;
+    private String senha;
+}
