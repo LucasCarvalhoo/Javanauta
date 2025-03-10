03b76a985d35e68e6eafee09f27cf38d7bb87145
Lucas Carvalho <lucas.carvalho7933@gmail.com>
2025-03-10 15:12:58 -0300

    implementa autenticação e autorização no projeto

--- src/main/java/com/carvalho/aprendendospring/infrastructure/entity/Usuario.java
+++ src/main/java/com/carvalho/aprendendospring/infrastructure/entity/Usuario.java
@@ -4,7 +4,10 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Data
@@ -12,7 +15,7 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 @Table(name = "usuario")
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,7 +24,7 @@ public class Usuario {
     private String nome;
     @Column(name = "email", length = 200)
     private String email;
-    @Column(name = "senha", length = 20)
+    @Column(name = "senha")
     private String senha;
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "usuario_id", referencedColumnName = "id")
@@ -30,4 +33,18 @@ public class Usuario {
     @JoinColumn(name = "usuario_id", referencedColumnName = "id")
     private List<Telefone> telefones;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
